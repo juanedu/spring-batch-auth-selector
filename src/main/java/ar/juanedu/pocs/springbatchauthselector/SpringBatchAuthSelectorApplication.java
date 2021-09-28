ba6851f2c85160cf8692eb9680c45c78d33d493c
Juan Vicente <JVICENTE@prismamp.com>
2021-09-28 15:09:39 -0300

    Initial commit

---
+++ src/main/java/ar/juanedu/pocs/springbatchauthselector/SpringBatchAuthSelectorApplication.java
@@ -0,0 +1,13 @@
+package ar.juanedu.pocs.springbatchauthselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAuthSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAuthSelectorApplication.class, args);
+	}
+
+}
